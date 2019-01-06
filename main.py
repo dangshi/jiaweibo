c9064e1bfcbdb253714c5524784693c144253589
dangshi <869125870@qq.com>
2019-01-06 20:00:34 +0800

    structure

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_restful import Api
+from flask_cors import CORS
+from query import GQuery
+from test import HelloWorld
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+api = Api(app)
+api.add_resource(GQuery, "/query")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
